5d3de95cbdadb1f14d82572ff2bb5b87e40f8371
charithardh <k.charithardhreddy1994@gmail.com>
2021-06-08 11:03:41 +0530

    Added the screenshort to the mail project

--- SeleniumPrac/src/com/selenium/prac/BaseTest.java
+++ SeleniumPrac/src/com/selenium/prac/BaseTest.java
@@ -1,10 +1,15 @@
 package com.selenium.prac;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Properties;
 
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,9 +18,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.ProfilesIni;
+import org.openqa.selenium.io.FileHandler;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class BaseTest {
 
@@ -175,6 +182,29 @@ public class BaseTest {
 				element=driver.findElement(By.partialLinkText(orElemen.getProperty(locator)));
 		}
 		return element;
+	}
+	
+	public static void VerificationFailed(String FailedMessage) throws IOException {
+		test.log(LogStatus.FAIL, FailedMessage);
+		Date dt=new Date();
+		String dateFormat=dt.toString().replace(":", "_").replace(" ", "_")+".png";
+		File scrensht = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(scrensht, new File(projectPath+"//failurescreenshots//"+dateFormat));
+		test.log(LogStatus.INFO, "Screenshot-->"+test.addScreenCapture(projectPath+"//failurescreenshots//"+dateFormat));
+		
+	}
+
+	public static void VerificationPassed(String passMessage) {
+		test.log(LogStatus.PASS, passMessage);
+		
+	}
+
+	public static boolean verifyLinkEqualsTo(String expectedText) {
+		String actualText=driver.findElement(By.xpath("//a[text()='Customer Service'][@class='nav-a  ']")).getText();
+		if(expectedText.equals(actualText))
+		    return true;
+		else
+			return false;
 	}
 
 	
